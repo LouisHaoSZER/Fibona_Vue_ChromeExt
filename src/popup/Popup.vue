b5b366bf8655e59ab5c7b02bdddf92615781c34c
LouisHao <1477285752@qq.com>
2024-06-11 15:47:36 +0800

    feat: 新增popup翻译页面功能

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -5,39 +5,145 @@
 //   browser.runtime.openOptionsPage()
 // }
 
-const btnList = [
-  {
-    text: '弹窗',
-  },
+// 可选翻译的语言
+const languages = [
+  { code: 'af', name: 'Afrikaans' },
+  { code: 'sq', name: 'Albanian' },
+  { code: 'am', name: 'Amharic' },
+  { code: 'ar', name: 'Arabic' },
+  { code: 'hy', name: 'Armenian' },
+  { code: 'az', name: 'Azerbaijani' },
+  { code: 'eu', name: 'Basque' },
+  { code: 'be', name: 'Belarusian' },
+  { code: 'bn', name: 'Bengali' },
+  { code: 'bs', name: 'Bosnian' },
+  { code: 'bg', name: 'Bulgarian' },
+  { code: 'ca', name: 'Catalan' },
+  { code: 'ceb', name: 'Cebuano' },
+  { code: 'ny', name: 'Chichewa' },
+  { code: 'zh', name: 'Chinese (Simplified)' },
+  { code: 'zh-TW', name: 'Chinese (Traditional)' },
+  { code: 'co', name: 'Corsican' },
+  { code: 'hr', name: 'Croatian' },
+  { code: 'cs', name: 'Czech' },
+  { code: 'da', name: 'Danish' },
+  { code: 'nl', name: 'Dutch' },
+  { code: 'en', name: 'English' },
+  { code: 'eo', name: 'Esperanto' },
+  { code: 'et', name: 'Estonian' },
+  { code: 'tl', name: 'Filipino' },
+  { code: 'fi', name: 'Finnish' },
+  { code: 'fr', name: 'French' },
+  { code: 'fy', name: 'Frisian' },
+  { code: 'gl', name: 'Galician' },
+  { code: 'ka', name: 'Georgian' },
+  { code: 'de', name: 'German' },
+  { code: 'el', name: 'Greek' },
+  { code: 'gu', name: 'Gujarati' },
+  { code: 'ht', name: 'Haitian Creole' },
+  { code: 'ha', name: 'Hausa' },
+  { code: 'haw', name: 'Hawaiian' },
+  { code: 'iw', name: 'Hebrew' },
+  { code: 'hi', name: 'Hindi' },
+  { code: 'hmn', name: 'Hmong' },
+  { code: 'hu', name: 'Hungarian' },
+  { code: 'is', name: 'Icelandic' },
+  { code: 'ig', name: 'Igbo' },
+  { code: 'id', name: 'Indonesian' },
+  { code: 'ga', name: 'Irish' },
+  { code: 'it', name: 'Italian' },
+  { code: 'ja', name: 'Japanese' },
+  { code: 'jw', name: 'Javanese' },
+  { code: 'kn', name: 'Kannada' },
+  { code: 'kk', name: 'Kazakh' },
+  { code: 'km', name: 'Khmer' },
+  { code: 'ko', name: 'Korean' },
+  { code: 'ku', name: 'Kurdish (Kurmanji)' },
+  { code: 'ky', name: 'Kyrgyz' },
+  { code: 'lo', name: 'Lao' },
+  { code: 'la', name: 'Latin' },
+  { code: 'lv', name: 'Latvian' },
+  { code: 'lt', name: 'Lithuanian' },
+  { code: 'lb', name: 'Luxembourgish' },
+  { code: 'mk', name: 'Macedonian' },
+  { code: 'mg', name: 'Malagasy' },
+  { code: 'ms', name: 'Malay' },
+  { code: 'ml', name: 'Malayalam' },
+  { code: 'mt', name: 'Maltese' },
+  { code: 'mi', name: 'Maori' },
+  { code: 'mr', name: 'Marathi' },
+  { code: 'mn', name: 'Mongolian' },
+  { code: 'my', name: 'Myanmar (Burmese)' },
+  { code: 'ne', name: 'Nepali' },
+  { code: 'no', name: 'Norwegian' },
+  { code: 'ps', name: 'Pashto' },
+  { code: 'fa', name: 'Persian' },
+  { code: 'pl', name: 'Polish' },
+  { code: 'pt', name: 'Portuguese' },
+  { code: 'pa', name: 'Punjabi' },
+  { code: 'ro', name: 'Romanian' },
+  { code: 'ru', name: 'Russian' },
+  { code: 'sm', name: 'Samoan' },
+  { code: 'gd', name: 'Scots Gaelic' },
+  { code: 'sr', name: 'Serbian' },
 ]
 
-// function injectScript() {
-//   browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//     chrome.tabs.sendMessage(tabs[0].id, {
-//       action: 'injectScript',
-//       script: 'alert("Hello, World!")',
-//     })
-//   })
-// }
+// 翻译函数脚本
+function translateScript() {
+  chrome.runtime.onMessage.addListener((request) => {
+    if (request.language) {
+      const translateApi = `https://translate.google.com/translate?hl=${request.language}&sl=auto&tl=${request.language}&u=${window.location.href}`
+      window.location.href = translateApi
+    }
+  })
+}
+
+// 监听翻译按钮点击事件
+function handleTranslate() {
+  const language = document.getElementById('language').value
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      func: translateScript,
+    }).then(() => {
+      chrome.tabs.sendMessage(tabs[0].id, { language })
+    })
+  })
+}
 </script>
 
 <template>
   <main class="p-3 w-50">
-    <div>
-      <div
-        class="p-3  text-center text-sm font-bold"
+    <div
+      class=" text-xl font-bold mb-3 pb-1"
+      style="border-bottom: 1px solid black;"
+    >
+      翻译当前页
+    </div>
+    <label
+      class="block text-gray-700 text-sm font-bold mb-2"
+      for="language"
+    >选择翻译的语言</label>
+    <select
+      id="language"
+      class="block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded  mb-3 leading-tight focus:outline-none focus:bg-white" name=""
+    >
+      <option
+        v-for="(lang, index) in languages"
+        :key="index"
+        :value="lang.code"
       >
-        请选择注入的脚本
-      </div>
-      <div class="flex flex-col space-y-2">
-        <button
-          v-for="btn in btnList"
-          :key="btn.text"
-          class="btn btn-primary"
-        >
-          {{ btn.text }}
-        </button>
-      </div>
+        {{ lang.name }}
+      </option>
+    </select>
+    <div class="mt-5 text-center">
+      <button
+        id="translate"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        @click="handleTranslate"
+      >
+        翻译
+      </button>
     </div>
   </main>
 </template>
